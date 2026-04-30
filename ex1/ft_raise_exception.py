e6dce17cf1f5ea326ae0b3d7af05c0da72f71b59
fratojo <felixfotsiny@gmail.com>
2026-04-30 15:55:32 +0300

    first commit

--- ex1/ft_raise_exception.py
+++ ex1/ft_raise_exception.py
@@ -1,22 +1,27 @@
 # ************************************************************************* #
 #                                                                           #
 #                                                      :::      ::::::::    #
-#  ft_first_exception.py                             :+:      :+:    :+:    #
+#  ft_raise_exception.py                             :+:      :+:    :+:    #
 #                                                  +:+ +:+         +:+      #
 #  By: maratojo <maratojo@student.42.fr>         +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
-#  Created: 2026/04/29 11:37:20 by maratojo        #+#    #+#               #
-#  Updated: 2026/04/30 14:30:41 by maratojo        ###   ########.fr        #
+#  Created: 2026/04/29 13:39:56 by maratojo        #+#    #+#               #
+#  Updated: 2026/04/30 14:15:08 by maratojo        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
 def input_temperature(temp_str: str) -> int:
-    return int(temp_str)
+    temp = int(temp_str)
+    if temp < 0:
+        raise ValueError(f"{temp}°C is too cold for plants (min 0°C)")
+    elif temp > 40:
+        raise ValueError(f"{temp}°C is too hot for plants (max 40°C)")
+    return temp
 
 
 def test_temperature() -> None:
     print("=== Garden Temperature ===")
-    test_case = ["25", "abc"]
+    test_case = ["25", "abc", "100", "-50"]
     for test in test_case:
         print(f"\nInput data is '{test}'")
         try:
@@ -25,6 +30,5 @@ def test_temperature() -> None:
         except ValueError as e:
             print(f"Caught input_temperature error: {e}")
     print("\nAll tests completed - program didn't crash!")
-
 
 test_temperature()
---
+++ ex2/ft_different_errors.py
@@ -0,0 +1,19 @@
+# ************************************************************************* #
+#                                                                           #
+#                                                      :::      ::::::::    #
+#  ft_different_errors.py                            :+:      :+:    :+:    #
+#                                                  +:+ +:+         +:+      #
+#  By: maratojo <maratojo@student.42.fr>         +#+  +:+       +#+         #
+#                                              +#+#+#+#+#+   +#+            #
+#  Created: 2026/04/30 15:05:03 by maratojo        #+#    #+#               #
+#  Updated: 2026/04/30 15:29:31 by maratojo        ###   ########.fr        #
+#                                                                           #
+# ************************************************************************* #
+
+def garden_operations(operation_number):
+    result =
+    if operation_number == 0:
+        int("abc")
+    elif operation_number == 1:
+        result = 10 / 0
+    elif
